d35abb83715270a353f4454ed2108d9b701479ef
Ryan Simmons <ryansimmons112782@gmail.com>
2016-04-08 22:22:18 -0700

    added jQuery and bootstrap to header

--- header.php
+++ header.php
@@ -2,7 +2,15 @@
 <html>
 	<head>
 		<title>ASU Capstone Portal</title>
+
+		<!-- CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+		<!-- Scripts -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<script src="https://code.jquery.com/jquery-2.2.3.min.js"></script>
+
 	</head>
-	
+
 	<body>
 
