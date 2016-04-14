2de73e5dd2c188d26ddd09d71b15998658fef2e1
Ryan Simmons <ryansimmons112782@gmail.com>
2016-04-13 23:49:27 -0700

    added stuff to index

--- index.php
+++ index.php
@@ -1,6 +1,137 @@
-<?
-require_once "header.php";
-echo "hey";
-require_once "footer.php";
+<?require_once "header.php";?>
 
-?>
+<div id="wrapper">
+
+    <!-- Sidebar -->
+    <div id="sidebar-wrapper">
+        <ul class="sidebar-nav">
+            <li class="sidebar-brand"><a href="#">ASU Capstone Portal</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Sign Out</a></li>
+        </ul>
+    </div>
+    <!-- /#sidebar-wrapper -->
+
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                	<h1>Dashboard</h1>
+
+                	<div class="row">
+                		<div class="col-lg-4">
+							<div class="panel panel-default" id='student-profile'>
+								<div class="panel-heading">
+									<h3 class="panel-title">Profile</h3>
+								</div>
+								<div class="panel-body">
+									Panel content
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="panel panel-default" id='student-profile'>
+								<div class="panel-heading">
+									<h3 class="panel-title">Profile</h3>
+								</div>
+								<div class="panel-body">
+									Panel content
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="panel panel-default" id='student-profile'>
+								<div class="panel-heading">
+									<h3 class="panel-title">Profile</h3>
+								</div>
+								<div class="panel-body">
+									Panel content
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+                		<div class="col-lg-4">
+							<div class="panel panel-default" id='student-profile'>
+								<div class="panel-heading">
+									<h3 class="panel-title">Profile</h3>
+								</div>
+								<div class="panel-body">
+									Panel content
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="panel panel-default" id='student-profile'>
+								<div class="panel-heading">
+									<h3 class="panel-title">Profile</h3>
+								</div>
+								<div class="panel-body">
+									Panel content
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="panel panel-default" id='student-profile'>
+								<div class="panel-heading">
+									<h3 class="panel-title">Profile</h3>
+								</div>
+								<div class="panel-body">
+									Panel content
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+                		<div class="col-lg-4">
+							<div class="panel panel-default" id='student-profile'>
+								<div class="panel-heading">
+									<h3 class="panel-title">Profile</h3>
+								</div>
+								<div class="panel-body">
+									Panel content
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="panel panel-default" id='student-profile'>
+								<div class="panel-heading">
+									<h3 class="panel-title">Profile</h3>
+								</div>
+								<div class="panel-body">
+									Panel content
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4">
+							<div class="panel panel-default" id='student-profile'>
+								<div class="panel-heading">
+									<h3 class="panel-title">Profile</h3>
+								</div>
+								<div class="panel-body">
+									Panel content
+								</div>
+							</div>
+						</div>
+					</div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+
+
+
+<?require_once "footer.php";?>
