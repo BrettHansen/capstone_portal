b7568b01114ecc08d937ffe4d2e953f073b6a71c
Ryan Simmons <ryansimmons112782@gmail.com>
2016-04-20 21:52:21 -0700

    student-teams.php added

---
+++ student-teams.php
@@ -0,0 +1,201 @@
+<?require_once "header.php";?>
+
+<div id="wrapper">
+
+    <!-- Sidebar -->
+    <div id="sidebar-wrapper">
+        <ul class="sidebar-nav">
+            <li class="sidebar-brand"><a href="#">ASU Capstone Portal</a></li>
+            <li><a href="#">Profile</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Teams</a></li>
+            <!-- <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li> -->
+            <li><a href="#">Sign Out</a></li>
+        </ul>
+    </div>
+    <!-- /#sidebar-wrapper -->
+
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                	<h1>Team Selection
+                	<button class="btn btn-default btn-lg">Create New Team</button></h1>
+                	<div class="row">
+                		<div class="col-lg-3">
+							<div class="panel panel-success" id='student-profile'>
+								<div class="panel-heading">
+									<div class="row">
+										<h2 class="panel-title col-lg-9">Team 1 (Open)</h2>
+										<button class="btn btn-success btn-sm col-lg-2">Join</button>
+									</div>
+								</div>
+								<div class="panel-body">
+									<table class="table table-striped">
+										<tbody>
+											<tr>
+												<td>James Aaberg</td>
+											</tr>
+											<tr>
+												<td>John Aaron</td>
+											</tr>
+											<tr>
+												<td>Robert Abarca</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-3">
+							<div class="panel panel-success" id='student-profile'>
+								<div class="panel-heading">
+									<div class="row">
+										<h2 class="panel-title col-lg-9">Team 2 (Open)</h2>
+										<button class="btn btn-success btn-sm col-lg-2">Join</button>
+									</div>
+								</div>
+								<div class="panel-body">
+									<table class="table table-striped">
+										<tbody>
+											<tr>
+												<td>Michael Abbate</td>
+											</tr>
+											<tr>
+												<td>Mary Abbess</td>
+											</tr>
+											<tr>
+												<td>William Abbott</td>
+											</tr>
+											<tr>
+												<td>David Abbratozzato</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-3">
+							<div class="panel panel-success" id='student-profile'>
+								<div class="panel-heading">
+									<div class="row">
+										<h2 class="panel-title col-lg-9">Team 3 (Open)</h2>
+										<button class="btn btn-success btn-sm col-lg-2">Join</button>
+									</div>
+								</div>
+								<div class="panel-body">
+									<table class="table table-striped">
+										<tbody>
+											<tr>
+												<td>Richard Abdelnour</td>
+											</tr>
+											<tr>
+												<td>Charles Abderrazzaq</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-3">
+							<div class="panel panel-info" id='student-profile'>
+								<div class="panel-heading">
+									<div class="row">
+										<h2 class="panel-title col-lg-9">Team 4 (Private)</h2>
+										<button class="btn btn-info btn-sm col-lg-2">Request</button>
+									</div>
+								</div>
+								<div class="panel-body">
+									<table class="table table-striped">
+										<tbody>
+											<tr>
+												<td>Joseph Abdollah</td>
+											</tr>
+											<tr>
+												<td>Thomas Abdous</td>
+											</tr>
+											<tr>
+												<td>Patricia Abdullah</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-lg-3">
+							<div class="panel panel-info" id='student-profile'>
+								<div class="panel-heading">
+									<div class="row">
+										<h2 class="panel-title col-lg-9">Team 5 (Private)</h2>
+										<button class="btn btn-info btn-sm col-lg-2">Request</button>
+									</div>
+								</div>
+								<div class="panel-body">
+									<table class="table table-striped">
+										<tbody>
+											<tr>
+												<td>Christopher Abdulrazak</td>
+											</tr>
+											<tr>
+												<td>Linda Abe</td>
+											</tr>
+											<tr>
+												<td>Barbara Abel</td>
+											</tr>
+											<tr>
+												<td>Daniel Abelmann</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-3">
+							<div class="panel panel-danger" id='student-profile'>
+								<div class="panel-heading">
+									<div class="row">
+										<h2 class="panel-title col-lg-9">Team 6 (Full)</h2>
+									</div>
+								</div>
+								<div class="panel-body">
+									<table class="table table-striped">
+										<tbody>
+											<tr>
+												<td>Paul Abelson</td>
+											</tr>
+											<tr>
+												<td>Mark Abernathy</td>
+											</tr>
+											<tr>
+												<td>Elizabeth Abnet</td>
+											</tr>
+											<tr>
+												<td>Donald Abraham</td>
+											</tr>
+											<tr>
+												<td>Jennifer Abraham-Scalapino</td>
+											</tr>
+											<tr>
+												<td>George Abrams</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>		
+				</div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+
+<?require_once "footer.php";?>
