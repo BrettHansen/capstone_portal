5aaa56eb7f2099593cc55d9df520aca152a9b385
Ryan Simmons <ryansimmons112782@gmail.com>
2016-04-20 22:55:23 -0700

    login page added

--- index.php
+++ index.php
@@ -3,10 +3,26 @@
 <div id="page-content-wrapper">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-12" style="text-align: center;">
-            	<button class='btn btn-default'><a href='student-dashboard.php'>Student</a></button>
-            	<button class='btn btn-default'><a href='sponser-dashboard.php'>Sponser</a></button>
-            	<button class='btn btn-default'><a href='instructor-dashboard.php'>Instructor</a></button>
+            <div class="col-lg-12">
+            	<h1 class="text-center">ASU Capstone Portal</h1>
+            	<div class="panel panel-default col-lg-4 col-lg-offset-4" id='login'>
+            		<div class="panel-body">
+            			<form role="form">
+							<div class="form-group">
+								<label for="name">Username</label>
+								<input type="name" class="form-control" id="name">
+							</div>
+							<div class="form-group">
+								<label for="password">Password</label>
+								<input type="password" class="form-control" id="password">
+							</div>
+							<button type="submit" class="btn btn-default">Submit</button>
+							<button class='btn btn-default'><a href='student-dashboard.php'>Student</a></button>
+					    	<button class='btn btn-default'><a href='sponser-dashboard.php'>Sponser</a></button>
+					    	<button class='btn btn-default'><a href='instructor-dashboard.php'>Instructor</a></button>
+						</form>
+					</div>
+				</div>
             </div>
         </div>
     </div>
