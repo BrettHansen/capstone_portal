0f0e026e2796df59084b1abd9a0dd853f79f44e6
Ryan Simmons <ryansimmons112782@gmail.com>
2016-05-01 02:51:49 -0700

    can 'add team'

--- student-teams.php
+++ student-teams.php
@@ -10,7 +10,7 @@
             <div class="row">
                 <div class="col-lg-12">
                 	<h1>Team Selection
-                	<button class="btn btn-default btn-lg">Create New Team</button></h1>
+                	<button class="btn btn-default btn-lg" onclick="addTeam()">Create New Team</button></h1>
                 	<div class="row">
                 		<div class="col-lg-3">
 							<div class="panel panel-success" id='student-profile'>
@@ -105,7 +105,7 @@
 							</div>
 						</div>
 					</div>
-					<div class="row">
+					<div class="row" id="team-panels">
 						<div class="col-lg-3">
 							<div class="panel panel-info" id='student-profile'>
 								<div class="panel-heading">
@@ -163,6 +163,62 @@
 								</table>
 							</div>
 						</div>
+
+						<div hidden class='col-lg-3' id='newTeam'>
+							<div class='panel panel-warning' id='student-profile'>
+								<div class='panel-heading'>
+									<div class='row'>
+										<h2 class='panel-title col-lg-9'>New Team</h2><br/>
+										<label class='radio-inline'><input type='radio' name='radio' value='Private'>Private</label>
+										<label class='radio-inline'><input type='radio' name='radio' value='Public'>Public</label>
+										<button class='btn btn-default' onclick='saveTeam()'>Save</button>
+									</div>
+								</div>
+								<table class='table table-striped'>
+									<tbody>
+										<tr>
+											<td><input class='form-control name' id='p1'></td>
+										</tr>
+										<tr>
+											<td><input class='form-control name' id='p2'></td>
+										</tr>
+										<tr>
+											<td><input class='form-control name' id='p3'></td>
+										</tr>
+										<tr>
+											<td><input class='form-control name' id='p4'></td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+
+						<div hidden class="col-lg-3" id="savedTeam">
+							<div class="panel panel-success" id='student-profile'>
+								<div class="panel-heading">
+									<div class="row">
+										<h2 class="panel-title col-lg-9">Team 7 (Open)</h2>
+										<button class="btn btn-success btn-sm col-lg-2">Join</button>
+									</div>
+								</div>
+								<table class="table table-striped">
+									<tbody id="newNames">
+										<tr>
+											<td id='np1'></td>
+										</tr>
+										<tr>
+											<td id='np2'></td>
+										</tr>
+										<tr>
+											<td id='np3'></td>
+										</tr>
+										<tr>
+											<td id='np4'></td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
 					</div>		
 				</div>
             </div>
@@ -171,6 +227,24 @@
 
 </div>
 
+<script>
+	function saveTeam() {
+		$('#np1').text($('#p1').val());
+		$('#np2').text($('#p2').val());
+		$('#np3').text($('#p3').val());
+		$('#np4').text($('#p4').val());
+		$('#np5').text($('#p5').val());
+		$('#np6').text($('#p6').val());
 
+		$('#newTeam').hide();
+
+
+		$('#savedTeam').show();
+
+	}
+	function addTeam() {	
+		$('#newTeam').show();
+	}
+</script>
 
 <?require_once "footer.php";?>
