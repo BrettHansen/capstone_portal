cf32cd13c4b185f94194d9a9d35fe48a510c2cb2
Brett Hansen <hansen.brett7@gmail.com>
2016-09-25 23:29:34 -0700

    All teams now viewable.

--- view_teams.php
+++ view_teams.php
@@ -1,4 +1,29 @@
-<?require_once "header.php";?>
+<?
+require('includes/config.php');
+require_once("header.php");
+
+$db = new Database();
+$students_query = $db->query("SELECT * FROM student_details");
+$teams_query = $db->query("SELECT * FROM teams");
+
+$students = array();
+foreach ($students_query as $value) {
+	$students[$value["id"]] = $value["name"];
+}
+
+$teams = array();
+foreach($teams_query as $value) {
+	$teams[$value["id"]] = array();
+	$teams[$value["id"]]["proposal_id"] = $value["proposal_id"];
+	$teams[$value["id"]]["members"] = array();
+}
+
+foreach($students_query as $value) {
+	if(array_key_exists($value["team_id"] , $teams)) {
+		array_push($teams[$value["team_id"]]["members"], $value["id"]);
+	}
+}
+?>
 
 <div id="wrapper">
 
@@ -10,225 +35,37 @@
                 <div class="col-lg-12">
                 	<h1>Teams</h1>
                 	<div class="row">
-                		<div class="col-lg-3">
-							<div class="panel panel-default" id='student-profile'>
-								<div class="panel-heading">
-									<div class="row">
-										<h2 class="panel-title col-lg-9">No Team</h2>
-									</div>
-								</div>
-								<table class="table table-striped">
-									<tbody>
-										<tr>
-											<td>Maria Abramson</td>
-										</tr>
-										<tr>
-											<td>Kenneth Abromson-Leeman</td>
-										</tr>
-										<tr>
-											<td>Susan Aburdene</td>
-										</tr>
-										<tr>
-											<td>Steven Acambages</td>
-										</tr>
-										<tr>
-											<td>Edward Accardo</td>
-										</tr>
-										<tr>
-											<td>Margaret Accomazzi</td>
-										</tr>
-										<tr>
-											<td>Brian Achacoso</td>
-										</tr>
-										<tr>
-											<td>Ronald Acker</td>
-										</tr>
-										<tr>
-											<td>Dorothy Ackerly</td>
-										</tr>
-										<tr>
-											<td>Anthony Ackerman</td>
-										</tr>
-										<tr>
-											<td>Lisa Acus</td>
-										</tr>
-										<tr>
-											<td>Kevin Adair</td>
-										</tr>
-										<tr>
-											<td>Nancy Adam</td>
-										</tr>
-										<tr>
-											<td>Karen Adame</td>
-										</tr>
-										<tr>
-											<td>Betty Adams</td>
-										</tr>
-										<tr>
-											<td>Helen Adamson</td>
-										</tr>
-										<tr>
-											<td>Jason Addison</td>
-										</tr>
-										<tr>
-											<td>Matthew Ade</td>
-										</tr>
-										<tr>
-											<td>Gary Adelson</td>
-										</tr>
-									</tbody>
-								</table>
-							</div>
-						</div>
-
 						<div class="col-lg-9">
 		                	<div class="row">
-		                		<div class="col-lg-3">
-									<div class="panel panel-success" id='student-profile'>
-										<div class="panel-heading">
-											<div class="row">
-												<h2 class="panel-title col-lg-9">Team 1 (Open)</h2>
+		                		<?php 
+		                		foreach ($teams as $team_id => $team) {
+		                		?>
+			                		<div class="col-lg-3">
+										<div class="panel panel-success" id='student-profile'>
+											<div class="panel-heading">
+												<div class="row">
+													<h2 class="panel-title col-lg-9">Team <?php echo $team_id; ?></h2>
+												</div>
 											</div>
+											<table class="table table-striped">
+												<tbody>
+													<?php
+													foreach ($team["members"] as $member) {
+														$name = $students[$member];
+													?>
+														<tr>
+															<td><?php echo $name; ?></td>
+														</tr>
+													<?php
+													}
+													?>
+												</tbody>
+											</table>
 										</div>
-										<table class="table table-striped">
-											<tbody>
-												<tr>
-													<td>James Aaberg</td>
-												</tr>
-												<tr>
-													<td>John Aaron</td>
-												</tr>
-												<tr>
-													<td>Robert Abarca</td>
-												</tr>
-											</tbody>
-										</table>
 									</div>
-								</div>
-								<div class="col-lg-3">
-									<div class="panel panel-success" id='student-profile'>
-										<div class="panel-heading">
-											<div class="row">
-												<h2 class="panel-title col-lg-9">Team 2 (Open)</h2>
-											</div>
-										</div>
-										<table class="table table-striped">
-											<tbody>
-												<tr>
-													<td>Michael Abbate</td>
-												</tr>
-												<tr>
-													<td>Mary Abbess</td>
-												</tr>
-												<tr>
-													<td>William Abbott</td>
-												</tr>
-												<tr>
-													<td>David Abbratozzato</td>
-												</tr>
-											</tbody>
-										</table>
-									</div>
-								</div>
-								<div class="col-lg-3">
-									<div class="panel panel-success" id='student-profile'>
-										<div class="panel-heading">
-											<div class="row">
-												<h2 class="panel-title col-lg-9">Team 3 (Open)</h2>
-											</div>
-										</div>
-										<table class="table table-striped">
-											<tbody>
-												<tr>
-													<td>Richard Abdelnour</td>
-												</tr>
-												<tr>
-													<td>Charles Abderrazzaq</td>
-												</tr>
-											</tbody>
-										</table>
-									</div>
-								</div>
-								<div class="col-lg-3">
-									<div class="panel panel-info" id='student-profile'>
-										<div class="panel-heading">
-											<div class="row">
-												<h2 class="panel-title col-lg-9">Team 4 (Private)</h2>
-											</div>
-										</div>
-										<table class="table table-striped">
-											<tbody>
-												<tr>
-													<td>Joseph Abdollah</td>
-												</tr>
-												<tr>
-													<td>Thomas Abdous</td>
-												</tr>
-												<tr>
-													<td>Patricia Abdullah</td>
-												</tr>
-											</tbody>
-										</table>
-									</div>
-								</div>
-							</div>
-							<div class="row">
-								<div class="col-lg-3">
-									<div class="panel panel-info" id='student-profile'>
-										<div class="panel-heading">
-											<div class="row">
-												<h2 class="panel-title col-lg-9">Team 5 (Private)</h2>
-											</div>
-										</div>
-										<table class="table table-striped">
-											<tbody>
-												<tr>
-													<td>Christopher Abdulrazak</td>
-												</tr>
-												<tr>
-													<td>Linda Abe</td>
-												</tr>
-												<tr>
-													<td>Barbara Abel</td>
-												</tr>
-												<tr>
-													<td>Daniel Abelmann</td>
-												</tr>
-											</tbody>
-										</table>
-									</div>
-								</div>
-								<div class="col-lg-3">
-									<div class="panel panel-danger" id='student-profile'>
-										<div class="panel-heading">
-											<div class="row">
-												<h2 class="panel-title col-lg-9">Team 6 (Full)</h2>
-											</div>
-										</div>
-										<table class="table table-striped">
-											<tbody>
-												<tr>
-													<td>Paul Abelson</td>
-												</tr>
-												<tr>
-													<td>Mark Abernathy</td>
-												</tr>
-												<tr>
-													<td>Elizabeth Abnet</td>
-												</tr>
-												<tr>
-													<td>Donald Abraham</td>
-												</tr>
-												<tr>
-													<td>Jennifer Abraham-Scalapino</td>
-												</tr>
-												<tr>
-													<td>George Abrams</td>
-												</tr>
-											</tbody>
-										</table>
-									</div>
-								</div>
+								<?php
+								}
+								?>
 							</div>
 						</div>
 					</div>	
