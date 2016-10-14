c43ecee3294f011fdf38cefbf0f0d6873871fd68
Brett Hansen <hansen.brett7@gmail.com>
2016-10-14 08:48:19 -0700

    Added view projects page.

--- sidebar.php
+++ sidebar.php
@@ -4,7 +4,8 @@
         <li><a href="dashboard.php">Dashboard</a></li>
         <? if($_SESSION['user_role'] != 2) {?><li><a href="#">Profile</a></li><?}?>
         <? if($_SESSION['user_role'] == 3) {?><li><a href="proposal_form.php">New Proposal</a></li><?}?>
-        <? if($_SESSION['user_role'] != 3) {?><li><a href="view_proposals.php">Projects</a></li><?}?>
+        <? if($_SESSION['user_role'] != 2) {?><li><a href="view_proposals.php">Proposals</a></li><?}?>
+        <? if($_SESSION['user_role'] != 3) {?><li><a href="view_projects.php">Projects</a></li><?}?>
         <? if($_SESSION['user_role'] != 3) {?><li><a href="#">Students</a></li><?}?>
         <? if($_SESSION['user_role'] != 3) {?><li><a href="view_teams.php">Teams</a></li><?}?>
         <? if($_SESSION['user_role'] == 2) {?><li><a href="#">Settings</a></li><?}?>
